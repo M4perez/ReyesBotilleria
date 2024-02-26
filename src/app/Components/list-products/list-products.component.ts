25ca29f6eef61d6e3c2df0c3ca809f7d033a3f77
Mperez <xTemplerx@gmail.com>
2024-02-26 14:03:56 -0300

    se crea ruta para add-edit

--- src/app/Components/list-products/list-products.component.ts
+++ src/app/Components/list-products/list-products.component.ts
@@ -6,9 +6,11 @@ import { Product } from 'src/app/Interfaces/product';
   templateUrl: './list-products.component.html',
   styleUrls: ['./list-products.component.css']
 })
+//ITERACION DE ITEMS SE CREA ARRAY LIST PRODUCTS
 export class ListProductsComponent {
   listProducts: Product[] = [
     { name: 'CocaCola', description: 'Bebida Azucarada', price: 2000, stock: 100 },
     { name: 'Corona', description: 'Bebida alcoholica', price: 1500, stock: 200 },
+    { name: 'Cigarros Kent', description: 'Tabaco mezclado con veneno de rata', price: 5000, stock: 200 },
   ]
 }
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-// CREA Y EXPORTA LAS RUTAS DE LOS MODULOS
-const routes: Routes = [];
+// COMPONENTES
+
+import { ListProductsComponent } from './Components/list-products/list-products.component';
+import { AddEditProductComponent } from './Components/add-edit-product/add-edit-product.component';
+
+// RUTAS DE LOS MODULOS
+const routes: Routes = [
+  { path: '', component: ListProductsComponent },
+  { path: 'add-edit', component: AddEditProductComponent },
+  { path: '**', redirectTo: '', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
